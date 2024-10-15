370f2bc4bd23b17d4dced7e114e14c5490b65651
adityasingh <adityasingh1803001@gmail.com>
2024-10-15 17:16:27 +0530

    Commit

---
+++ Tasks/Chapter 2 Tasks/Program 1/script.js
@@ -0,0 +1,9 @@
+// Use logical operator to find weather the age of a person lies between 10 and 20
+
+let age = prompt("Enter Your Age : ");
+if( age>10 && age<20) {
+    console.log("Your age is Lies Between 10 and 20");
+}
+else{
+    console.log("Your Age is Doesn't lies Between 10 and 20");
+}
---
+++ Tasks/Chapter 2 Tasks/Program 2/script.js
@@ -0,0 +1,55 @@
+// Switch Case 
+
+// let a = prompt("Enter Your Name to Find Best Sibling : ");
+
+// switch (a) {
+//     case "Aditya": 
+//         console.log("Lucky");
+//         break;
+
+//     case "Lucky": 
+//         console.log("Kunal");
+//         break;
+
+//     case "Kunal": 
+//         console.log("Monu");
+//         break;
+    
+//     case "Sonu": 
+//         console.log("Shilpa");
+//         break;
+
+//     case "Abhishek": 
+//         console.log("Vicky");
+//         break;
+
+//     case "Vicky": 
+//         console.log("Kunal");
+//         break;
+
+//     case "Monu": 
+//         console.log("Paisa");
+//         break;
+
+//     default:
+//         console.log("Humare Khandan ka Nhi");
+        
+//         break;
+// }
+
+
+let a1=prompt("Enter Your Name to Find Your Favourite Dish")
+
+switch (a1) {
+    case "Abhishek":
+        console.log("Lund Chusta With Gravy");
+        break;
+
+    case "Lucky":
+        console.log("Aad Bhat");
+        
+        break;
+    default:
+
+        break;
+}
