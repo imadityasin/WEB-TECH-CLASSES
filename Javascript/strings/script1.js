391825bf977fcdce2754e60a93ca98fbe40924c4
adityasingh <adityasingh1803001@gmail.com>
2024-11-04 05:40:50 +0530

    hh

---
+++ Javascript/strings/script1.js
@@ -0,0 +1,26 @@
+let name = "Aditya Singh";
+
+
+console.log(name.length);
+
+
+console.log(name.toUpperCase())
+
+
+console.log(name.toLowerCase())
+
+
+console.log(name.slice(0, 3))
+
+
+console.log(name.slice(3))
+
+
+console.log(name.replace("Singh", "Rajput"))
+
+
+let friend = "VISHNU"
+console.log(name.toUpperCase().concat(" is a friend of ", friend, " ok !!"))
+
+let friend2 = "           SHIV           "
+console.log(friend2)
